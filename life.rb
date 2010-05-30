53c51396269ed0f57cb884aae9b75061f74bb0f5
James Brechtel <jbrechtel@gmail.com>
2010-05-30 10:08:46 -0400

    added a parser

--- life.rb
+++ life.rb
@@ -1,19 +1,32 @@
 require 'lib/life'
 
+class Parser
+  def self.world_from(world_s)
+    World.new(world_s.split(/\n/).map do |line|
+      line.split(/ /).map do |cell_s|
+        Cell.new((cell_s == '#' ? :alive : :dead))
+      end
+    end)
+  end
+end
+
 dead_cell = Cell.new :dead
 
-world = World.new [
-                    [Cell.new, Cell.new, Cell.new],
-                    [Cell.new, Cell.new, dead_cell],
-                    [Cell.new, Cell.new, Cell.new]
-                  ]
+world = Parser.world_from %{
+# - # - # - # # #
+- # # # # - - # -
+# - # - # - # # -
+# # # # # - - # -
+- - # - # - # - #
+}
 
-puts world.to_s
+  puts world.to_s
 
-10.times do
+100.times do
   f = Fate.new(world)
   world = World.new(world.rows.map { |row| row.map { |cell| f.of(cell) } })
   puts "\n\n"
   puts world.to_s
 end
+
 
