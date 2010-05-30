ebdd509c17de70346b24c5c66c5c22b33495501d
James Brechtel <jbrechtel@gmail.com>
2010-05-30 10:22:56 -0400

    Fixing output position on creen

--- life.rb
+++ life.rb
@@ -13,20 +13,20 @@ end
 dead_cell = Cell.new :dead
 
 world = Parser.world_from %{
-# - # - # - # # #
-- # # # # - - # -
-# - # - # - # # -
-# # # # # - - # -
-- - # - # - # - #
+# - # - # - # # # - #
+- # # # # - - # - - #
+# - # - # - # # - - #
+# # # # # - - # - - #
+- - # - # - # - # - #
 }
 
-  puts world.to_s
 
 100.times do
   f = Fate.new(world)
   world = World.new(world.rows.map { |row| row.map { |cell| f.of(cell) } })
-  puts "\n\n"
-  puts world.to_s
+  puts "\e[H\e[2J"
+  printf "\r#{world}"
+  sleep(1)
 end
 
 
